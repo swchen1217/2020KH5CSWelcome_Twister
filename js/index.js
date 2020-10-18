3a829a66aa7ae2c2f188deef83ce4528d774196d
Sz-Wei Chen <swchen1217@gmail.com>
2020-10-19 00:27:02 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 var default_color = "#b5b5b5"
+var flash_color = "#ffffff"
 var colors = ["#ea4335", "#fbbc05", "#4285f4", "#34a853"];
 var times = 20;
-var delay = 75;
+var times_spin = 20;
+var delay_spin = 75;
+var times_flash = 3;
+var delay_flash = 250;
 var score = 0;
 var colorStatus = [[null, null, null, null], [null, null, null, null], [null, null, null, null]];
 var autoEnable = false;
@@ -21,8 +25,10 @@ function reset() {
 }
 
 function single(id) {
-    colorStatus[id - 1][Math.floor(Math.random() * 4)] = Math.floor(Math.random() * 4);
-    animation(id);
+    var p = Math.floor(Math.random() * 4);
+    colorStatus[id - 1][p] = Math.floor(Math.random() * 4);
+    animation_spin(id);
+    animation_flash(id, p + 1);
 }
 
 function auto() {
@@ -38,15 +44,28 @@ function auto() {
         single(i);
 }
 
-function animation(id) {
-    for (let i = 0; i < times; i++) {
+function animation_spin(id) {
+    for (let i = 0; i < times_spin; i++) {
         window.setTimeout(function () {
             for (var k = 4; k > 0; k--) {
-                if (i != times - 1)
+                if (i != times_spin - 1)
                     document.getElementById("c" + id + "_" + k).style.backgroundColor = colors[(i - k + 2) % 4];
                 else
                     document.getElementById("c" + id + "_" + k).style.backgroundColor = colors[colorStatus[id - 1][k - 1]] ?? default_color;
             }
-        }, delay * i);
+        }, delay_spin * i);
     }
+}
+
+function animation_flash(id, position) {
+    window.setTimeout(function () {
+        for (let i = 0; i < times_flash * 2; i++) {
+            window.setTimeout(function () {
+                if (i % 2 == 0)
+                    document.getElementById("c" + id + "_" + position).style.backgroundColor = flash_color;
+                else
+                    document.getElementById("c" + id + "_" + position).style.backgroundColor = colors[colorStatus[id - 1][position - 1]] ?? default_color;
+            }, delay_flash / 2 * i);
+        }
+    }, delay_spin * times_spin);
 }
